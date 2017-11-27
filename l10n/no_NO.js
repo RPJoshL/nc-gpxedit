38a9caae58d415f0de45443a9a34e610d707ba44
Julien Veyssier <eneiluj@posteo.net>
2017-11-27 23:06:15 +0100

    gen new translations

---
+++ l10n/no_NO.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "gpxedit",
+    {
+    "GpxEdit" : "GpxEdit",
+    "Delete" : "Slett",
+    "Load in GpxEdit" : "Last inn GpxEdit",
+    "Edit with GpxEdit" : "Redigér med GpxEdit",
+    "Draw a track" : "Lag et spor",
+    "Add a waypoint" : "Legg til et veipunkt",
+    "Edit" : "Redigér",
+    "Nothing to edit" : "Ingenting å redigere",
+    "Nothing to delete" : "Ingenting å slette"
+},
+"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: gpxedit\nX-Crowdin-Language: no");
