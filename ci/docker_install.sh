31168d4c985992181a5927d0bd626e120750409e
Julien Veyssier <eneiluj@posteo.net>
2018-01-14 19:25:28 +0100

    test CI

---
+++ ci/docker_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+apt-get update -yqq
+apt-get install git -yqq
+
+curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+chmod +x /usr/local/bin/phpunit
+
+cd /data/apps/
+git clone https://gitlab.com/eneiluj/gpxedit-oc gpxedit
+
+ls /data
+ls /data/apps
