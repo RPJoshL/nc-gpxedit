4436dd22faf57cf2c0b8be36fb0d3dbba59f39c4
Julien Veyssier <eneiluj@posteo.net>
2018-01-15 12:03:03 +0100

    CICI

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -16,9 +16,10 @@ chmod +x /usr/local/bin/phpunit
 cd /var/www/html/custom_apps/
 git clone https://gitlab.com/eneiluj/gpxedit-oc gpxedit
 
-sudo -u www-data php /var/www/html/occ
-#sudo -u www-data php /var/www/html/occ app:enable gpxedit
-#sudo -u www-data php /var/www/html/occ upgrade
-#sudo -u www-data php /var/www/html/occ maintenance:mode --off
+sudo -u www-data php /var/www/html/occ maintenance:install --database "sqlite" --admin-user "admin" --admin-pass "password"
+sudo -u www-data php /var/www/html/occ app:enable gpxedit
+sudo -u www-data php /var/www/html/occ upgrade
+sudo -u www-data php /var/www/html/occ maintenance:mode --off
+sudo -u www-data php /var/www/html/occ app:check-code gpxedit
 
 ls /var/www/html/custom_apps/
