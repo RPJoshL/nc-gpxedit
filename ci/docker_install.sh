d3154f70ac249b918cfef542cd89ef916d4fd65d
Julien Veyssier <eneiluj@posteo.net>
2018-01-15 11:42:15 +0100

    CICI

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -11,13 +11,12 @@ apt-get install git sudo -yqq
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
 
-mkdir /data/apps -p
-
-cd /data/apps/
+#mkdir /data/apps -p
+#cd /data/apps/
+cd /var/www/html/custom_apps/
 git clone https://gitlab.com/eneiluj/gpxedit-oc gpxedit
 
-sudo -u www-data php occ upgrade
-sudo -u www-data php occ maintenance:mode --off
+sudo -u www-data php /var/www/html/occ upgrade
+sudo -u www-data php /var/www/html/occ maintenance:mode --off
 
-ls /data
-ls /data/apps
+ls /var/www/html/custom_apps/
