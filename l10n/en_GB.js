aac4961568d03688c269cbfecea38b406b49d0d6
Julien Veyssier <eneiluj@posteo.net>
2018-10-16 19:15:14 +0200

    write new translations

---
+++ l10n/en_GB.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "gpxedit",
+    {
+    "GpxEdit" : "GpxEdit"
+},
+"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: gpxedit\nX-Crowdin-Language: en-GB");
--- l10n/write.sh
+++ l10n/write.sh
@@ -22,3 +22,5 @@ rename 's/sk_SK/sk/g' *.js*
 rename 's/fa_IR/fa/g' *.js*
 rename 's/hi_IN/hi/g' *.js*
 rename 's/id_ID/id/g' *.js*
+rename 's/bg_BG/bg/g' *.js*
+rename 's/en_US/en/g' *.js*
