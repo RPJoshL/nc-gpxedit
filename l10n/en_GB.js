c20048317a4658c804fd759f5ccbb91d9a087e54
Julien Veyssier <eneiluj@posteo.net>
2019-04-12 08:04:55 +0200

    update translation system

--- l10n/en_GB.js
+++
@@ -1,6 +0,0 @@
-OC.L10N.register(
-    "gpxedit",
-    {
-    "GpxEdit" : "GpxEdit"
-},
-"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: gpxedit\nX-Crowdin-Language: en-GB");
