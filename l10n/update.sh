df2eb6ef52c59888f92f264c1de5a21d532f804b
Julien Veyssier <eneiluj@posteo.net>
2019-04-12 10:31:37 +0200

    add update.sh

---
+++ l10n/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git checkout l10n_master
+git reset --hard HEAD~200
+git pull origin l10n_master
+git rebase master
+git reset --soft master
+git commit -m "new translations from crowdin"
+git checkout master
+echo "ready to merge l10n_master"
