9270050dfc0e4d42863a67bb2570d32d08a85ab2
RPJosh <RPJosh@rpjosh.de>
2024-07-07 13:16:29 +0200

    Remove deprecated maps and add google satellite

--- lib/Controller/TileServers.php
+++ lib/Controller/TileServers.php
@@ -70,51 +70,11 @@ $baseTileServers = [
         'maxzoom' => '18'
     ),
     Array(
-        'name' => 'Hike & bike',
-        'type' => 'tile',
-        'url' => 'http://a.tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png',
-        'attribution' => 'Map data &copy; 2013 <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-        'minzoom' => '1',
-        'maxzoom' => '18'
-    ),
-    Array(
-        'name' => 'OpenStreetMap France',
-        'type' => 'tile',
-        'url' => 'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
-        'attribution' => 'Map data &copy; 2013 <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-        'minzoom' => '1',
-        'maxzoom' => '18'
-    ),
-    Array(
-        'name' => 'IGN France',
-        'type' => 'tile',
-        'url' => 'http://wxs.ign.fr/ljthe66m795pr2v2g8p7faxt/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS' .
-            '&EXCEPTIONS=text/xml&FORMAT=image/jpeg' .
-            '&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal' .
-            '&TILEMATRIXSET=PM&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}',
-        'attribution' => '&copy; <a href="http://www.ign.fr/">IGN-France</a>',
-        'minzoom' => '1',
-        'maxzoom' => '18'
-    ),
-    Array(
         'name' => 'Dark',
         'type' => 'tile',
         'url' => 'http://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
         'attribution' => '&copy; Map tiles by CartoDB, under CC BY 3.0. Data by' .
                          ' OpenStreetMap, under ODbL.',
-        'minzoom' => '1',
-        'maxzoom' => '18'
-    ),
-    Array(
-        'name' => 'WaterColor',
-        'type' => 'tile',
-        'url' => 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',
-        'attribution' => '<a href="http://leafletjs.com" title="A JS library' .
-        ' for interactive maps">Leaflet</a> | © Map tiles by <a href="http://stamen' .
-        '.com">Stamen Design</a>, under <a href="http://creativecommons.org/license' .
-        's/by/3.0">CC BY 3.0</a>, Data by <a href="http://openstreetmap.org">OpenSt' .
-        'reetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0"' .
-        '>CC BY SA</a>.',
         'minzoom' => '1',
         'maxzoom' => '18'
     ),
@@ -130,6 +90,24 @@ $baseTileServers = [
         '>CC BY SA</a>.',
         'minzoom' => '1',
         'maxzoom' => '18'
+    ),
+    Array(
+        'name' => 'Google Maps Sattellite',
+        'type' => 'tile',
+        'url' => 'https://mt.google.com/vt?lyrs=s&x={x}&y={y}&z={z}',
+        'attribution' => "<a href='http://maps.google.com/'>Google Maps</a> Satellite",
+        'minzoom' => '1',
+        'maxzoom' => '20'
+
+    ),
+    Array(
+        'name' => 'Google Maps Hybrid',
+        'type' => 'tile',
+        'url' => 'https://mt.google.com/vt?lyrs=y&x={x}&y={y}&z={z}',
+        'attribution' => "<a href='http://maps.google.com/'>Google Maps</a> Hybrid",
+        'minzoom' => '1',
+        'maxzoom' => '20'
+
     ),
     Array(
         'name' => 'OsmFr Route500',
